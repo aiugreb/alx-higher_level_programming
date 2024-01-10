6819405e1865123934b22d988f31047e4f8e9316
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-10 14:10:17 -0800

    debug

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key in list (a_dictionary.keys()):
-        if a_dictionary[key] == value:
+    for key, v in list(a_dictionary.items()):
+        if v == value:
             del a_dictionary[key]
-            return a_dictionary
+    return a_dictionary
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,17 +6,17 @@ def roman_to_int(roman_string):
         return 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50,
             "C": 100, "D": 500, "M": 1000}
-    nextt = ""
+    nextR = ""
     num = 0
     for i in range(len(roman_string)):
-        if roman_string[i] in roman.keys():
+        if roman_string[i] in roman_nums.keys():
             key = roman_string[i]
             if i < len(roman_string) - 1:
-                nextt = roman_string[i + 1]
+                nextR = roman_string[i + 1]
             if i == len(roman_string) - 1:
-                num += roman[key]
-            elif roman[key] < roman[nextt]:
-                num -= roman[key]
+                num += roman_nums[key]
+            elif roman_nums[key] < roman_nums[nextR]:
+                num -= roman_nums[key]
             else:
-                num += roman[key]
-        return num
+                num += roman_nums[key]
+    return num
