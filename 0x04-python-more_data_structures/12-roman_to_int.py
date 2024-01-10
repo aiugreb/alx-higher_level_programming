0db202bccfbade7ab8a88b462a7a474ad4508c16
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-10 09:07:15 -0800

    debug task 7 and task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,13 @@ def roman_to_int(roman_string):
         return 0
     if (isinstance(roman_string, str)) is False:
         return 0
-    roman = {"I": 1, "V": 5, "X": 10, "L": 50,
-            "C": 100, "D": 500, "M": 1000}
+    roman = {"I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000}
     nextR = ""
     num = 0
     for i in range(len(roman_string)):
