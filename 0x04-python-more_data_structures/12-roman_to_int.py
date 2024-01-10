ad4c028d8de46b4ee2f3aabf83c71467bc523955
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-10 14:13:18 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
-        return 0
-    if (isinstance(roman_string, str)) is False:
-        return 0
-    roman = {"I": 1, "V": 5, "X": 10, "L": 50,
-            "C": 100, "D": 500, "M": 1000}
-    nextR = ""
-    num = 0
-    for i in range(len(roman_string)):
-        if roman_string[i] in roman_nums.keys():
-            key = roman_string[i]
-            if i < len(roman_string) - 1:
-                nextR = roman_string[i + 1]
-            if i == len(roman_string) - 1:
-                num += roman_nums[key]
-            elif roman_nums[key] < roman_nums[nextR]:
-                num -= roman_nums[key]
-            else:
-                num += roman_nums[key]
-    return num
+    sum = 0
+    index = 0
+    if roman_string is not None:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+        listrom = list(roman.keys())
+        length = len(roman_string)
+        for char in roman_string:
+            # print("++ "+char)
+            if char in roman:
+                # print("+ "+char, roman[char])
+                if char in 'IXC':
+                    if index < length-1:
+                        if listrom[index+1] in 'VXLCDM':
+                            sum = sum + (roman[listrom[index+1]]-roman[char])
+                sum += roman[char]
+            index += 1
+    return sum
