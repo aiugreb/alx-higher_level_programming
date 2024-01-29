2c90337885b8b15fa2a228fafab3e52bb4eb16f8
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-29 12:41:08 -0800

    debug task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -5,13 +5,11 @@
 class Rectangle:
     """Create rectangle class."""
 
-
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle."""
-
 
         type(self).number_of_instances += 1
         self.width = width
