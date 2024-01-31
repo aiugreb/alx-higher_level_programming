cfc2f121a107928393a979f15c808f153c474489
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-31 06:18:32 -0800

    debugg

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,7 +11,3 @@ def add_integer(a, b=98):
     elif not (type(b) is float or type(b) is int):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
