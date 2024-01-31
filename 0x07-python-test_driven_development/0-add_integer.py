de3996201ff6fba802725d3bcf28913478da3fea
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-31 06:57:50 -0800

    0-add_integer.py debugg

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,11 +5,13 @@ Function that adds 2 integers
 
 """
 def add_integer(a, b=98):
-    """
-    These comments are stupid
-    """
-    if not (type(a) is float or type(a) is int):
+    """Return the addition of two numbers."""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    elif not (type(b) is float or type(b) is int):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
