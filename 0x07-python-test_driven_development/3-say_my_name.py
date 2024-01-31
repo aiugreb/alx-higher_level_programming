264deed6aae4f5173b268b03d145d51bd76defc9
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-31 07:21:08 -0800

    3-say_my_name debugg

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """
-
 Function that prints "My name is <first name> <last name>"
-
 """
 
 
 def say_my_name(first_name, last_name=""):
     """
         Function that prints "My name is <first name> <last name>"
-
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
@@ -18,7 +15,3 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     print("My name is {:s} {:s}".format(first_name, last_name))
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
