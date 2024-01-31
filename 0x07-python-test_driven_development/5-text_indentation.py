d87fc12c3c6d551058482e57a4a27852854c11f2
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-31 07:54:36 -0800

    debugg task5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
-Function that prints a text with 2 new lines after each '.', '?', ':'
-"""
+"""Function that prints a text with 2 new lines after each '.', '?', ':'"""
 
 
 def text_indentation(text):
+     """Print text with two new lines after each '.', '?', and ':'."""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     i = 0
     while i < len(text) and text[i] == ' ':
         i += 1
