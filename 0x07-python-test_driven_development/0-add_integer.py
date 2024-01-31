094a42f2a52fa692c59ee4cd71ce5cbc7cf4bbfe
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-31 07:00:42 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@ Function that adds 2 integers
 """
 
 
-def add_integer(a, b=98):
+def add_integer(a, b):
     """Return the addition of two numbers."""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
