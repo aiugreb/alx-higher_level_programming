b88c6a102854b2d620c715ad133123f76d58a817
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-02-05 11:33:10 -0800

    INHERITANCEwq

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Base geometry module """
+
+
+class BaseGeometry:
+    """Represent base geometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
