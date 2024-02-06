8eebdb89ee381e5add99d8476be5d57708c12edf
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-02-06 07:25:06 -0800

    task7

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Def a write_file function."""
 
+
 def write_file(filename="", text=""):
     """ func """
     with open(filename, 'w', encoding="utf-8") as f:
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Def a write_file function."""
 
+
 def append_write(filename="", text=""):
     """ func """
     with open(filename, 'a', encoding="utf-8") as f:
