fba387ecfd21629edc04815bc3a7ea76da3329ed
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-03-12 15:16:13 -0700

    advanced tasks

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- import database dump temperatures.sql to hbtn_0c_0 database
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,5 @@
+-- displays the max temprature of each state ordered by ststename
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state ASC;
