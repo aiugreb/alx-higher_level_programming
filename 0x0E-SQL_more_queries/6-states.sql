4779ad1e5fa654a0eb4ee6b23cdcfb715cab883b
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-03-13 09:39:53 -0700

    task 5-6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates a database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+	name VARCHAR(256)
+    );
