152d57d3e497c3d8ce626538c5e3ac16cdfd2860
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-04-14 12:13:03 -0700

    mappingg debugg

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,7 +2,9 @@
 """lists all State objects from the database hbtn_0e_6_usa"""
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine,
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     usrname = sys.argv[1]
