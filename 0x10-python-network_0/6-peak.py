a6c16b70ddedfa2fd0e9f8fae9a0b21e0bde5ab6
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-04-25 18:05:32 -0700

    debugg

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,13 +4,19 @@
 def find_peak(list_of_integers):
     """Finds the peak"""
 
-    if list_of_integers:
-        lf = 0
-        r = len(list_of_integers) - 1
-        while lf < r:
-            m = (lf + r) // 2
-            if list_of_integers[m] > list_of_integers[m + 1]:
-                r = m
-            else:
-                lf = m + 1
-        return list_of_integers[lf]
+    list_ = list_of_integers
+    # if there is no list of integers return None
+    if list_ == []:
+        return None
+    length = len(list_)
+
+    start, end = 0, length - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
+            return list_[mid]
+        if list_[mid - 1] > list_[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return list_[start]
