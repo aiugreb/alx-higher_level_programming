5a362af41bae80530e544bb405141f401231b93e
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-04-26 08:54:32 -0700

    0-hbtn_status.pydebugg

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,9 @@
 - fetches https://alx-intranet.hbtn.io/status.
 - uses urlib package
 """
+import urllib.request
+
+
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
@@ -10,4 +13,3 @@ if __name__ == "__main__":
         utf_data = data.decode('utf-8')
         resType = type(data)
         print(f"Body response:\n\t- type: {resType}\n\t\
-- content: {data}\n\t- utf8 content: {utf_data}")
